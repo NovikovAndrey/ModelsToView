205c294b36cde3dd81af3c0b4970da4a5d957a39
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-18 21:06:36 +0300

    Add MVC Collections

---
+++ Collection/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Collection.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Collection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Game game = new Game
+            {
+                Name = "Wow", 
+                Paltform="PC", 
+                Engine="Unknown"
+            };
+            Game game1 = new Game
+            {
+                Name = "Wow1",
+                Paltform = "PC1",
+                Engine = "Unknown"
+            };
+            Game game2 = new Game
+            {
+                Name = "Wow2",
+                Paltform = "PC2",
+                Engine = "Unknown"
+            };
+            List<Game> games = new List<Game>();
+            games.Add(game);
+            games.Add(game1);
+            games.Add(game2);
+            return View(games);
+        }
+    }
+}
---
+++ Collection/Model/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Collection.Model
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Paltform { get; set; }
+        public string Engine { get; set; }
+    }
+}
