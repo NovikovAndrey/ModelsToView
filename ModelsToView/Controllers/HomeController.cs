09ebc398dbcc636ebeeeccf47ae3ec3d3aa72c8a
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-18 20:57:43 +0300

    Add MVC SingleModel

---
+++ ModelsToView/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SingeModel.Model;
+
+namespace SingeModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Game game = new Game
+            {
+                Name="WOW", 
+                Platform="PC", 
+                Engine="Unknown"
+            };
+            return View(game);
+        }
+    }
+}
